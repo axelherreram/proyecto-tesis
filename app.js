0a3eb7e6de4121912177de5493460bdeb3e63ea2
axelherreram <aherreram22@miumg.edu.gt>
2024-07-24 21:12:17 -0600

    actualizacion con la nuevas rutas

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const taskRoutes = require('./routes/taskRoutes');
 const initializetables = require('./config/initializetables');
 const tesisAssigRoutes = require('./routes/tesisAssigRoutes');
 const fileRoutes = require('./routes/fileRoutes');
+const userRoutes = require('./routes/userRoutes');
 const cors = require('cors');
 
 require('dotenv').config();
@@ -23,9 +24,10 @@ app.use('/auth', authRoutes);
 app.use('/api', taskRoutes);
 app.use('/api', tesisAssigRoutes);
 app.use('/api', fileRoutes);
+app.use('/api', userRoutes);
+  
 
-
-sequelize.sync({ alter: true }) // , force: true  eliminar las tablas si existen
+sequelize.sync({ alter: true,  force: false}) // , force: true  eliminar las tablas si existen
   .then(async () => {
     console.log('Base de datos sincronizada');
     await initializetables(); 
