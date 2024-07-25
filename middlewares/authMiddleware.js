70209f6994cbf106fad34ef17588d1a507b72bef
axelherreram <aherreram22@miumg.edu.gt>
2024-07-24 21:09:41 -0600

    ajuste en la configuracion

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -18,4 +18,18 @@ const authMiddleware = (req, res, next) => {
   }
 };
 
-module.exports = authMiddleware;
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;  
+    next();
+  });
+};
+
+
+module.exports = authMiddleware, authenticateToken;
